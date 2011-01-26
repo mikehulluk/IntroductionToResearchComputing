a0df523bb3376186c5a4e01612a034b33afeb3a4
Mike Hull <mikehulluk@googlemail.com>
2011-01-26 22:10:40 +0000

    Wk8. Example1

---
+++ Wk8/samples/1_ConvertToCSV.py
@@ -0,0 +1,10 @@
+import datetime
+
+f = fdate = None
+for line in open("1_testdata.txt"):
+  if not line: continue
+  date,value = line.strip().rsplit(" ",1)
+  d = datetime.datetime.strptime(date.strip(),"%Y-%m-%d %H:%M:%S")
+  if fdate  is None: fdate = d
+  t_elapsed = (d - fdate).total_seconds()
+  print "%d, %s"%(t_elapsed, value)
